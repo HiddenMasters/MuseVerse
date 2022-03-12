396d3afbe8a4b2a37b1c84825f04a000f2cc366b
Junsu Min <sunsuking@gmail.com>
2022-03-13 05:08:13 +0900

    Fix PhotonNetwork

---
+++ Assets/Scripts/Photon/PhotonCamera.cs
@@ -0,0 +1,13 @@
+using System;
+using Photon.Pun;
+
+public class PhotonCamera : MonoBehaviourPun
+{
+    private void Start()
+    {
+        if (this.photonView.IsMine)
+        {
+            
+        }
+    }
+}
